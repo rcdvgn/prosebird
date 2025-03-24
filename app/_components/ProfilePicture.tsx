36c5acd2819af4cd9de561238a036655fc09ce98
Ricardo Vigliano <ricardorpvigliano@gmail.com>
2025-03-24 10:48:32 -0300

    profilePicture: color palette change

--- app/_components/ProfilePicture.tsx
+++ app/_components/ProfilePicture.tsx
@@ -27,7 +27,7 @@ const ProfilePicturePlaceholder = ({ className, firstName, lastName }: any) => {
   return (
     <div
       className={`aspect-square rounded-full flex items-center justify-center flex-shrink-0 min-w-0 text-primary font-semibold [&>div]:grid [&>div]:place-items-center ${className}`}
-      style={{ backgroundColor: `hsl(${nameColorHue}, 64%, 51%)` }}
+      style={{ backgroundColor: `hsl(${nameColorHue}, 80%, 50%)` }}
     >
       <ResponsiveFontSize
         ratio={0.4}
