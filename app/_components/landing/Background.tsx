dd7a6ad6d7c333c623c6f7eaf4fd4efc2819c2d6
Ricardo Vigliano <ricardorpvigliano@gmail.com>
2025-04-25 13:49:46 -0300

    landing: background fix attempt n.5

--- app/_components/landing/Background.tsx
+++ app/_components/landing/Background.tsx
@@ -1,13 +1,28 @@
-"use client";
-
 import Image from "next/image";
 
 export default function Background() {
   return (
     <div className="fixed top-0 left-0 h-screen w-screen pointer-events-none">
       <div className="relative h-full w-full">
-        <div className="absolute bg-brand rounded-full m-auto h-[65vw] w-[65vw] left-[-32.5vw] bottom-[-32.5vw] blur-[40vw]"></div>
-        <div className="absolute bg-brand rounded-full m-auto h-[65vw] w-[65vw] right-[-32.5vw] top-[-32.5vw] blur-[40vw]"></div>
+        {/* Bottom left blob with responsive sizing and blur */}
+        <div
+          className="absolute bg-brand rounded-full m-auto 
+          h-[90vh] w-[90vh] left-[-45vh] bottom-[-45vh] blur-[45vh]
+          sm:h-[110vh] sm:w-[110vh] sm:left-[-55vh] sm:bottom-[-55vh] sm:blur-[60vh]
+          md:h-[120vh] md:w-[120vh] md:left-[-60vh] md:bottom-[-60vh] md:blur-[70vh]
+          lg:h-[130vh] lg:w-[130vh] lg:left-[-65vh] lg:bottom-[-65vh] lg:blur-[80vh]
+          opacity-70 sm:opacity-80 md:opacity-90 lg:opacity-100"
+        ></div>
+
+        {/* Top right blob with responsive sizing and blur */}
+        <div
+          className="absolute bg-brand rounded-full m-auto 
+          h-[90vh] w-[90vh] right-[-45vh] top-[-45vh] blur-[45vh]
+          sm:h-[110vh] sm:w-[110vh] sm:right-[-55vh] sm:top-[-55vh] sm:blur-[60vh]
+          md:h-[120vh] md:w-[120vh] md:right-[-60vh] md:top-[-60vh] md:blur-[70vh]
+          lg:h-[130vh] lg:w-[130vh] lg:right-[-65vh] lg:top-[-65vh] lg:blur-[80vh]
+          opacity-70 sm:opacity-80 md:opacity-90 lg:opacity-100"
+        ></div>
 
         <Image
           width={1534}
--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${publicSans.className}`}>
+      <body className={`bg-background ${publicSans.className}`}>
         <PostHogProvider> {children}</PostHogProvider>
       </body>
     </html>
